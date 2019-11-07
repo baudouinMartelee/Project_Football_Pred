af5c5b79e24492c7acd1e9ae4301f5aec1d7d6c0
mwilk1904 <37634251+mwilk1904@users.noreply.github.com>
2019-11-07 14:42:48 +0100

    function engineering

--- clean.py
+++ clean.py
@@ -1,13 +1,25 @@
 import pandas as pd
 from collections import Counter
 
-matchs = pd.read_csv('X_Train.csv')
+matchsTrain = pd.read_csv('X_Train.csv')
+matchsTest = pd.read_csv('X_Test.csv')
 players = pd.read_csv('Player.csv')
 # countries = pd.read_csv('Country.csv')
 # leagues = pd.read_csv('League.csv')
-teams = pd.read_csv('Team.csv')
-team_attr = pd.read_csv('Team_Attributes.csv')
+# teams = pd.read_csv('Team.csv')
+# team_attr = pd.read_csv('Team_Attributes.csv')
 player_attr = pd.read_csv('Player_Attributes.csv')
+
+ply_attr = player_attr[['player_api_id', 'overall_rating', 'date']]
+ply_attr['date'] = ply_attr['date'].apply(lambda x: x.split('-')[0])
+# ply_attr['date_year'] = ply_attr['date'].apply(lambda x: x.split('-')[0])
+
+ply_attr = ply_attr.groupby(
+    [ply_attr['player_api_id'], ply_attr['date']]).mean()
+
+# Replace id of players with their overall rating at the date of the match
+
+ply_attr_dict = ply_attr.to_dict()['overall_rating']
 
 
 # Determine the label of the match (0: tie , 1: home team won, -1: home team lost)
@@ -21,23 +33,10 @@ def det_label(score1, score2):
 
 
 # Create labels
-matchs['label'] = matchs.apply(lambda row: det_label(
-    row.home_team_goal, row.away_team_goal), axis=1)
-
-# Droping irrelevent columns
-matchs.drop(['country_id', 'league_id', 'match_api_id',
-             'home_team_api_id', 'away_team_api_id', 'Unnamed: 0'], axis=1, inplace=True)
-
-######## FEATURES ENGINEERING ##############
-
-# Creating formations of the Y coordinates
-
-# Fill the missing coordinates with the most recurrent ones
-matchs = matchs.apply(
-    lambda x: x.fillna(x.value_counts().index[0]))
+matchsTrain['label'] = matchsTrain.apply(lambda row: det_label(
+    row['home_team_goal'], row['away_team_goal']), axis=1)
 
 
-# Create a formation with the Y coordinates
 def create_formation(row, home):
     list_test = list()  # We need a list for Counter
     # print(row)
@@ -54,39 +53,6 @@ def create_formation(row, home):
     return form
 
 
-matchs['home_form'] = matchs.apply(
-    lambda x: create_formation(x, True), axis=1)
-matchs['away_form'] = matchs.apply(
-    lambda x: create_formation(x, False), axis=1)
-
-matchs.drop(['home_player_Y1', 'home_player_Y2', 'home_player_Y3', 'home_player_Y4', 'home_player_Y5',
-             'home_player_Y6', 'home_player_Y7', 'home_player_Y8', 'home_player_Y9', 'home_player_Y10',
-             'home_player_Y11', 'away_player_Y1', 'away_player_Y2', 'away_player_Y3', 'away_player_Y4', 'away_player_Y5',
-             'away_player_Y6', 'away_player_Y7', 'away_player_Y8', 'away_player_Y9', 'away_player_Y10', 'away_player_Y11'], axis=1, inplace=True)
-
-matchs.drop(['home_player_X1', 'home_player_X2', 'home_player_X3', 'home_player_X4', 'home_player_X5',
-             'home_player_X6', 'home_player_X7', 'home_player_X8', 'home_player_X9', 'home_player_X10',
-             'home_player_X11', 'away_player_X1', 'away_player_X2', 'away_player_X3', 'away_player_X4', 'away_player_X5',
-             'away_player_X6', 'away_player_X7', 'away_player_X8', 'away_player_X9', 'away_player_X10', 'away_player_X11'], axis=1, inplace=True)
-
-# print(matchs['home_form'].value_counts())
-# print(matchs['away_form'].value_counts())
-
-# Cleaning the date (take only dd-mm-yyy)
-matchs['date'] = matchs['date'].apply(lambda x: x.split(' ')[0])
-
-ply_attr = player_attr[['player_api_id', 'overall_rating', 'date']]
-ply_attr['date'] = ply_attr['date'].apply(lambda x: x.split('-')[0])
-#ply_attr['date_year'] = ply_attr['date'].apply(lambda x: x.split('-')[0])
-
-ply_attr = ply_attr.groupby(
-    [ply_attr['player_api_id'], ply_attr['date']]).mean()
-
-# Replace id of players with their overall rating at the date of the match
-
-ply_attr_dict = ply_attr.to_dict()['overall_rating']
-
-
 def test_key(api_id, date):
     api_id = int(api_id)
     while True:
@@ -98,25 +64,6 @@ def test_key(api_id, date):
             date = str(date)
 
 
-for i in range(1, 12):
-    matchs['home_player_overall_'+str(i)] = matchs.apply(
-        lambda x: test_key(int(x['home_player_'+str(i)]), x['date'].split('-')[0]), axis=1)
-    matchs['away_player_overall_'+str(i)] = matchs.apply(
-        lambda x: test_key(int(x['away_player_'+str(i)]), x['date'].split('-')[0]), axis=1)
-
-
-matchs['home_team_overall'] = matchs.select(
-    lambda col: col.startswith('home_player_overall_'), axis=1).mean(axis=1)
-matchs['away_team_overall'] = matchs.select(
-    lambda col: col.startswith('away_player_overall_'), axis=1).mean(axis=1)
-
-matchs.drop(matchs.select(
-    lambda col: col.startswith('home_player'), axis=1), axis=1, inplace=True)
-
-matchs.drop(matchs.select(
-    lambda col: col.startswith('away_player'), axis=1), axis=1, inplace=True)
-
-
 def get_best_team(home_team, away_team):
     if(home_team > away_team):
         return 1
@@ -126,5 +73,65 @@ def get_best_team(home_team, away_team):
         return -1
 
 
-matchs['best_team'] = matchs.apply(lambda x: get_best_team(
-    x['home_team_overall'], x['away_team_overall']), axis=1)
+def engineering_data(matchs):
+
+    # Droping irrelevent columns
+    matchs.drop(['country_id', 'league_id', 'match_api_id',
+                 'home_team_api_id', 'away_team_api_id', 'Unnamed: 0'], axis=1, inplace=True)
+
+    ######## FEATURES ENGINEERING ##############
+
+    # Creating formations of the Y coordinates
+
+    # Fill the missing coordinates with the most recurrent ones
+    matchs = matchs.apply(
+        lambda x: x.fillna(x.value_counts().index[0]))
+
+    # Create a formation with the Y coordinates
+
+    matchs['home_form'] = matchs.apply(
+        lambda x: create_formation(x, True), axis=1)
+    matchs['away_form'] = matchs.apply(
+        lambda x: create_formation(x, False), axis=1)
+
+    matchs.drop(['home_player_Y1', 'home_player_Y2', 'home_player_Y3', 'home_player_Y4', 'home_player_Y5',
+                 'home_player_Y6', 'home_player_Y7', 'home_player_Y8', 'home_player_Y9', 'home_player_Y10',
+                 'home_player_Y11', 'away_player_Y1', 'away_player_Y2', 'away_player_Y3', 'away_player_Y4', 'away_player_Y5',
+                 'away_player_Y6', 'away_player_Y7', 'away_player_Y8', 'away_player_Y9', 'away_player_Y10', 'away_player_Y11'], axis=1, inplace=True)
+
+    matchs.drop(['home_player_X1', 'home_player_X2', 'home_player_X3', 'home_player_X4', 'home_player_X5',
+                 'home_player_X6', 'home_player_X7', 'home_player_X8', 'home_player_X9', 'home_player_X10',
+                 'home_player_X11', 'away_player_X1', 'away_player_X2', 'away_player_X3', 'away_player_X4', 'away_player_X5',
+                 'away_player_X6', 'away_player_X7', 'away_player_X8', 'away_player_X9', 'away_player_X10', 'away_player_X11'], axis=1, inplace=True)
+
+    # print(matchs['home_form'].value_counts())
+    # print(matchs['away_form'].value_counts())
+
+    # Cleaning the date (take only dd-mm-yyy)
+    matchs['date'] = matchs['date'].apply(lambda x: x.split(' ')[0])
+
+    for i in range(1, 12):
+        matchs['home_player_overall_'+str(i)] = matchs.apply(
+            lambda x: test_key(int(x['home_player_'+str(i)]), x['date'].split('-')[0]), axis=1)
+        matchs['away_player_overall_'+str(i)] = matchs.apply(
+            lambda x: test_key(int(x['away_player_'+str(i)]), x['date'].split('-')[0]), axis=1)
+
+    matchs['home_team_overall'] = matchs.select(
+        lambda col: col.startswith('home_player_overall_'), axis=1).mean(axis=1)
+    matchs['away_team_overall'] = matchs.select(
+        lambda col: col.startswith('away_player_overall_'), axis=1).mean(axis=1)
+
+    matchs.drop(matchs.select(
+        lambda col: col.startswith('home_player'), axis=1), axis=1, inplace=True)
+
+    matchs.drop(matchs.select(
+        lambda col: col.startswith('away_player'), axis=1), axis=1, inplace=True)
+
+    matchs['best_team'] = matchs.apply(lambda x: get_best_team(
+        x['home_team_overall'], x['away_team_overall']), axis=1)
+
+    return matchs
+
+
+matchsTrain = engineering_data(matchsTrain)
+matchsTest = engineering_data(matchsTest)
